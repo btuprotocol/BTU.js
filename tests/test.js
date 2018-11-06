9afc25be37d576d111cade0f56a941b1b93d53eb
Pierre Gilardin <pierre.gilardin@btu-protocol.com>
2018-11-06 18:07:43 +0100

    Sample code new methods

--- tests/test.js
+++ tests/test.js
@@ -13,6 +13,9 @@ btujs.resources.getHotelInformations('AMDLWPAR008', function(json) {
 
 btujs.agenda.getAvailableRooms('AMDLWPAR008', '2018-12-01', '2018-12-03').then(json => console.log(json)).catch(err => console.log(err))
 
-btujs.agenda.getAvailableRooms('AMDLWPAR008', '2018-12-01', '2018-12-03', function(json) {
-	console.log(json)
-});
+
+btujs.resources.getRoomInformations('AMDLWPAR008', 'BOC304806/30480602_97855825_0_1_0-NB', '2018-12-01', '2018-12-03').then(json => console.log(json)).catch(err => console.log(err))
+
+btujs.agenda.getReservationInformations('GPQLMU_0000').then(json => console.log(json)).catch(err => console.log(err))
+
+btujs.agenda.cancelReservation('GPQLMU_0000').then(json => console.log(json)).catch(err => console.log(err))
