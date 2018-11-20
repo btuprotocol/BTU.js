a3bbfba31e134e38af3667e85a9017c92651a981
vincentboulay <vincent@btu-protocol.com>
2018-11-20 14:34:05 +0100

    Add tests

--- tests/test.js
+++ tests/test.js
@@ -13,7 +13,21 @@ btujs.resources.getHotelInformations('AMDLWPAR008', function(json) {
 */
 
 btujs.agenda.getAvailableRooms('AMDLWPAR008', '2018-12-01', '2018-12-03').then(json => console.log(json)).catch(err => console.log(err))
+/*
+var user = new Object()
+user.firstName = 'toto'
+user.lastName = 'titi'
+user.phone = '+330644'
+user.cardNumber = '444'
+user.cardUser = 'xxxxx'
+user.ccCode = 'AX'
+user.ccv = '1234'
+user.email = 'toto@gmail.com'
+user.expireDate = '1218'
 
+btujs.agenda.createReservation('BOC304806', 'BOC304806/30480601_97855825_0_1_0-NB',
+                                '2018-11-24', '2018-11-25', user).then(json => console.log(json)).catch(err => console.log(err))
+*/
 /*
 btujs.resources.getRoomInformations('AMDLWPAR008', 'BOC304806/30480602_97855825_0_1_0-NB', '2018-12-01', '2018-12-03').then(json => console.log(json)).catch(err => console.log(err))
 
